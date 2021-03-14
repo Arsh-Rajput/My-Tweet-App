03732bfcaadb14a68d46c74103a6a0f48f71ccb5
Arsh-Rajput <nikhildgrt.nis@gmail.com>
2021-03-14 17:37:40 +0530

    no change

--- src/main/java/com/arsh/twitter/TwitterApplication.java
+++ src/main/java/com/arsh/twitter/TwitterApplication.java
@@ -2,9 +2,13 @@ package com.arsh.twitter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -16,5 +20,6 @@ public class TwitterApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TwitterApplication.class, args);
 	}
+	
 
 }
